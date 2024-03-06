e3f20736f4bd83074d8690078258406fd78a4f8a
Munjarin Bano <munjarin2004@gmail.com>
2024-03-06 19:13:22 +0530

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,229 @@
+let allMusic = [
+    { 
+        name :" Afreen Afreen",
+        artist: "Rahat Fateh Ali Khan",
+        img : "afreen afreen",
+        src: "afreen afreen"
+
+    },
+    { 
+        name :" ahde wafa",
+        artist: "Rahat Fateh Ali Khan",
+        img : "ahde wafa",
+        src: "ahde wafa"
+
+    },
+    { 
+        name :" Amaanat",
+        artist: "Nabeel Shaukat",
+        img : "amaanat",
+        src: "amaanat"
+
+    },
+    { 
+        name :" baarishein",
+        artist: "Anuv Jain",
+        img : "baarishein",
+        src: "baarishein"
+
+    },
+    { 
+        name :" Chaand Taara",
+        artist: "Rafay Israr",
+        img : "chaand taara",
+        src: "chaand taara"
+
+    },
+    { 
+        name :" Mai Diwaana",
+        artist: "Wajhi Farooki",
+        img : "chaudhary and sons",
+        src: "chaudhary and sons"
+
+    },
+    { 
+        name :" Chupke Chupke",
+        artist: "Ali Zafar & Nirmal Roy",
+        img : "chupke chupke",
+        src: "chupke chupke"
+
+    },
+    { 
+        name :"Fairy Tale",
+        artist: "Shibatain Khalid",
+        img : "fairy tale",
+        src: "fairy tale"
+
+    },
+    { 
+        name :" Falak TaK Chal",
+        artist: "Udit Narayan",
+        img : "falak tak chal",
+        src: "Falak Tak Chal"
+
+    },
+    { 
+        name :"Halki Halki Si Barsaat",
+        artist: "Sajj Bhatt",
+        img : "halki halki si barsaat",
+        src: "Halki Halki Si Barsaat"
+
+    },
+    { 
+        name :"Tu Hi haqeekat",
+        artist: "Javed Ali",
+        img : "humsafar",
+        src: "humsafar"
+
+    },
+    { 
+        name :" Ishq Murshid",
+        artist: "Ahmed Jahanzeb",
+        img : "ishq murshid",
+        src: "ishq murshid"
+
+    },
+    { 
+        name :"Jaane Tamanna",
+        artist: "Ahmed Jahanzeb",
+        img : "jaane tamanna",
+        src: "jaane tamanna"
+
+    },
+    { 
+        name :"ishqiyan",
+        artist: "Asim Azhar",
+        img : "ishqiyan",
+        src: "ishqiyan"
+
+    },
+    { 
+        name :"Kaali Jhhulfo Ko",
+        artist: "Rahat Fteh Ali Khan",
+        img : "kaali kaali",
+        src: "kaali kaali"
+
+    },
+    { 
+        name :"Kahani Suno",
+        artist: "Kaifi Khalil",
+        img : "kahani suno",
+        src: "kahani suno"
+
+    },
+    { 
+        name :" Khuda Aur Mohabbat",
+        artist: "Rahat Fateh Ali Khan",
+        img : "khuda aur mohabbat",
+        src: "khuda aur mohabbat"
+
+    },
+    { 
+        name :" Kuchh Ankahi",
+        artist: "Azaan Sami Khan",
+        img : "kuchh ankahi",
+        src: "kuchh ankahi"
+
+    },
+    { 
+        name :" Maahi",
+        artist: "Falak Shabbir",
+        img : "maahi",
+        src: "maahi"
+
+    },
+    { 
+        name :"Mein",
+        artist: "Asim Azhar",
+        img : "mein",
+        src: "Mein"
+
+    },
+    { 
+        name :"mere paas tum ho",
+        artist: "Rahat Fateh Ali Khan",
+        img : "mere paas tum ho",
+        src: "mere paas tum ho"
+
+    },
+    { 
+        name :" Mohabbat Tujhe Alvida",
+        artist: "Sahir Ali Bagga",
+        img : "mohabbat alvida",
+        src: "Mohabbat alvida"
+
+    },
+    { 
+        name :" Mohabbat Gumshuda Meri",
+        artist: "Sibte Hasan",
+        img : "mohabbat gumshuda meri",
+        src: "Mohabbat Gumshuda Meri"
+
+    },
+    { 
+        name :"mohabbat",
+        artist: "Aditya Rikhari",
+        img : "mohabbat",
+        src: "mohabbat"
+
+    },
+    { 
+        name :"paristaan",
+        artist: "Asim Azhar",
+        img : "paristaan",
+        src: "paristaan"
+
+    },
+    { 
+        name :"pyaar ke sudqey",
+        artist: "Mulaazim Hussain",
+        img : "pyaar ke sudqey",
+        src: "pyaar ke sudqey"
+
+    },
+    { 
+        name :" rabba",
+        artist: "Sajid- Wajid",
+        img : "rabba",
+        src: "rabba"
+
+    },
+    { 
+        name :"sukoon",
+        artist: "Hassan and Roshan",
+        img : "sukoon",
+        src: "sukoon"
+
+    },
+    { 
+        name :" teri yaad",
+        artist: "Aditya Rikhari",
+        img : "teri yaad",
+        src: "teri yaad"
+
+    },
+    { 
+        name :"Tu Hai Mera Dholna",
+        artist: "Sanay Irani",
+        img : "tu hai mera dholna",
+        src: "tu hai mera dholna"
+
+    },
+    { 
+        name :" urran",
+        artist: "Ashir Wazhat",
+        img : "urran",
+        src: "urran"
+
+    },
+    {
+        name:"Yaara Teri Meri Yaariyan",
+        artist:"Ali Zafar,Asim Azhar, Sahi Ali Bagga",
+        img:"yaara teri meri yaariyan",
+        src:"yaara teri meri yaariyan"
+    }
+
+ 
+   
+]
+
